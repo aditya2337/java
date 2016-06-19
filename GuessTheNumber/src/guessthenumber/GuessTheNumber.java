f6d615c7827c5eb5d250b5b992beed32df0458d7
aditya <androiditya@gmail.com>
2016-06-20 00:40:57 +0530

    #30daysofCode guess the number

--- GuessTheNumber/src/guessthenumber/GuessTheNumber.java
+++ GuessTheNumber/src/guessthenumber/GuessTheNumber.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package guessthenumber;
 
-/**
- *
- * @author GV
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumber {
 
-    /**
-     * @param args the command line arguments
-     */
+    int theNUMBER;
+    int max;
+    Scanner scanner = new Scanner(System.in);
+
+    public GuessTheNumber() {
+        Random rand = new Random();
+        max = 100;
+        theNUMBER = Math.abs(rand.nextInt()) % (max + 1);
+    }
+
+    public void play() {
+        while (true) {
+            int move = scanner.nextInt();
+            if (move > theNUMBER) {
+                System.out.println("you number is too big");
+            } else if (move < theNUMBER) {
+                System.out.println("you number is too small");
+            } else {
+                System.out.println("you got it bro!!");
+                break;
+            }
+        }
+    }
+
+    public static void howBigIsMyNumber(int x) {
+        if (x >= 0 && x <= 10) {
+            System.out.println("nuber is pretty small.");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
+        GuessTheNumber playgame = new GuessTheNumber();
+        System.out.println("Try and guess my impossible number. It is between 1"
+                + " 100.go ahead and type a number between 1 and 100. Thank you"
+                + "!!");
+        playgame.play();
     }
-    
+
 }
