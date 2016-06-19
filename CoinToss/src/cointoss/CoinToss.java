9019b07ead3e59740c00d5323b544ef84a47dc88
aditya <androiditya@gmail.com>
2016-06-19 23:24:17 +0530

    #30daysofCode Cointoss

--- CoinToss/src/cointoss/CoinToss.java
+++ CoinToss/src/cointoss/CoinToss.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cointoss;
 
-/**
- *
- * @author GV
- */
+import java.util.Random;
+
+
 public class CoinToss {
 
-    /**
-     * @param args the command line arguments
-     */
+    public String tossACoin() {
+        Random rand = new Random();
+        int toss = Math.abs(rand.nextInt()) % 2;
+        if( toss == 0){
+            return "HEADS";
+        } else {
+            return "TAILS";
+        }
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        CoinToss game = new CoinToss();
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
     }
     
 }
