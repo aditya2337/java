c5ba60cc25e452ed782a81d4887e561b899c5c0c
aditya <androiditya@gmail.com>
2016-06-19 15:34:06 +0530

    #30daysofCode first official cmmit

--- Car/src/car/Car.java
+++ Car/src/car/Car.java
@@ -10,6 +10,52 @@ package car;
  * @author GV
  */
 public class Car {
+    
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    
+    double weight = 4079;
+    
+    boolean isTheCarOn = false;
+    char condition = 'A';
+    String nameofCar = "Aditya";
+    
+    double maxFuel = 16;
+    double currentFuel = 8;
+    double mpg = 26.4;
+    
+    int numberofPeopleInCar = 1;
+    
+    public Car( int customMaxSpeed, double customWeight, boolean customIstheCarOn){
+        maxSpeed = customMaxSpeed;
+        weight = customWeight;
+        isTheCarOn = customIstheCarOn;
+    }
+    
+    public void printVariables() {
+        System.out.println("This is the max speed = " + maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+        System.out.println(nameofCar);
+        System.out.println(numberofPeopleInCar);
+    }
+    
+    public void upgradeMinSpeed() {
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed - 1;
+    }
+    
+    public void getIn(int numberofPeopleToGetIn) {
+        
+    }
+    
+    public void getOut(int numberofPeopleToGetOut) {
+        
+    }
+    
+    
 
     /**
      * @param args the command line arguments
